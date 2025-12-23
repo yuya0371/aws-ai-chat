6706c9b337ec32cf13d4ae0e5c94c50e726ae6b9
Yuya Osanai <osanaiyuya1229@gmail.com>
2025-12-23 11:35:23 +0900

    user.emailを直すだけにこんなに時間がかかるとは

--- src/pages/NewChat.tsx
+++ src/pages/NewChat.tsx
@@ -1,7 +1,9 @@
 export default function NewChat() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">AIチャットアプリ????</h1>
+      <h1 className="text-3xl font-bold underline">
+        AIチャットアプリ作ろうと思ってます
+      </h1>
     </div>
   );
 }
