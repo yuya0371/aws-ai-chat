4e5884969cf52a042dbfe666d3a9a226bfd73881
Yuya Osanai <0371yuya.1229@gmail.com>
2025-12-23 11:32:59 +0900

    ????

--- src/pages/NewChat.tsx
+++ src/pages/NewChat.tsx
@@ -1,7 +1,7 @@
 export default function NewChat() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">AIチャットアプリ</h1>
+      <h1 className="text-3xl font-bold underline">AIチャットアプリ????</h1>
     </div>
   );
 }
